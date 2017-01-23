e88c990bbd0b65f0f10d8b193a9f2e46a15df7b5
Aleksandr Karetnikov <karetnikov@rambler.ru>
2017-01-23 11:49:50 +0300

    work

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.background = pygame.image.load('images/background.bmp')
+        #self.background = pygame.image.load('images/background.bmp')
         self.ship_speed_factor = 5
         # Параметры пули
         self.bullet_speed_factor = 3
